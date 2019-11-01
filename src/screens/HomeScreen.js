4af4807c92211b1046f724f39c0ebec9b57f1fde
irwantowibowo1992 <irwantowibowo1992@hotmail.com>
2019-11-01 18:55:24 +0700

    create button

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import {Text, StyleSheet} from 'react-native';
+import {Text, StyleSheet, View, Button} from 'react-native';
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>Home Screen</Text>;
+  return (
+    <View>
+      <Text style={styles.text}>Home Screen</Text>
+      <Button
+        onPress={() => console.log('Button Pressed')}
+        title="Go To List"
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
