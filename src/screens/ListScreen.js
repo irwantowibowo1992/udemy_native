74035ea9ca1f3110206f1e5b629bcc3ed26b2bab
irwantowibowo1992 <irwantowibowo1992@hotmail.com>
2019-11-01 17:39:50 +0700

    FlatList with key and style property

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -4,27 +4,64 @@ import {View, Text, StyleSheet, FlatList} from 'react-native';
 const ListScreen = () => {
   //membuat array of object
   const friends = [
-    {name: 'Friend #1'},
-    {name: 'Friend #2'},
-    {name: 'Friend #3'},
-    {name: 'Friend #4'},
-    {name: 'Friend #5'},
-    {name: 'Friend #6'},
-    {name: 'Friend #7'},
-    {name: 'Friend #8'},
-    {name: 'Friend #9'},
+    {
+      name: 'Friend #1',
+      age: 26,
+    },
+    {
+      name: 'Friend #2',
+      age: 25,
+    },
+    {
+      name: 'Friend #3',
+      age: 27,
+    },
+    {
+      name: 'Friend #4',
+      age: 24,
+    },
+    {
+      name: 'Friend #5',
+      age: 19,
+    },
+    {
+      name: 'Friend #6',
+      age: 30,
+    },
+    {
+      name: 'Friend #7',
+      age: 28,
+    },
+    {
+      name: 'Friend #8',
+      age: 29,
+    },
+    {
+      name: 'Friend #9',
+    },
   ];
 
   return (
     <FlatList
+      keyExtractor={friend => friend.name}
       data={friends}
       renderItem={({item}) => {
-        return <Text>{item.name}</Text>;
+        return (
+          <Text style={styles.textStyle}>
+            {item.name} - Age {item.age}
+          </Text>
+        );
       }}
     />
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  textStyle: {
+    marginVertical: 50,
+    backgroundColor: 'green',
+    height: 40,
+  },
+});
 
 export default ListScreen;
