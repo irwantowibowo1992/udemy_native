5af01cf2571cbcb86d3522b2cd9756537de6f5d7
irwantowibowo1992 <irwantowibowo1992@hotmail.com>
2019-11-01 14:35:18 +0700

    contoh penulisan JSX

--- src/screens/ComponentScreen.js
+++ src/screens/ComponentScreen.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import {Text, StyleSheet} from 'react-native';
+import {Text, StyleSheet, View} from 'react-native';
 
 const ComponentScreen = () => {
-  return <Text style={styles.textStyle}>This is Component Screen</Text>;
+  const salam = 'Assalamualaikum';
+  const arai = ['asdg', 'bbb'];
+  const objek = {color: 'red'};
+  const nama = <Text>Irwanto Wibowo</Text>;
+
+  return (
+    <View>
+      <Text style={styles.textStyle}>This is Component Screen</Text>
+      <Text>{salam}</Text>
+      <Text>{arai}</Text>
+      <Text>{objek.color}</Text>
+      <Text>{nama}</Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
